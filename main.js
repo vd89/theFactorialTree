4f09e87f5f285b274ae6171b96bff0cb043244c4
vd89 <me@daxcodes.in>
2020-05-14 18:31:37 +0530

    the factorial tree.

---
+++ main.js
@@ -0,0 +1,41 @@
+var chart_config = {
+    chart: {
+        container: "#tree-simple"
+    },
+
+    nodeStructure: {
+        text: { name: "1" },
+        children: [
+            {
+                text: { name: "2" },
+                children: [
+                    {
+                        text: { name: "3" },
+                    },
+                    {
+                        text: { name: "3" },
+                    },
+                    {
+                        text: { name: "3" },
+                    }
+                ]
+            },
+            {
+                text: { name: "2" },
+                children: [
+                    {
+                        text: { name: "3" },
+                    },
+                    {
+                        text: { name: "3" },
+                    },
+                    {
+                        text: { name: "3" },
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+
