da74f289011b55434579acd95bf58296f9f49bc7
vd89 <me@daxcodes.in>
2020-05-15 14:11:12 +0530

    the change and getting the factorial tree

--- main.js
+++ main.js
@@ -4,122 +4,29 @@ var chart_config = {
   },
 
   nodeStructure: {
-    text: { name: '1' },
-    children: [
-      {
-        text: { name: '2' },
-        children: [
-          {
-            text: { name: '3' },
-            children: [
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-            ],
-          },
-          {
-            text: { name: '3' },
-            children: [
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-            ],
-          },
-          {
-            text: { name: '3' },
-            children: [
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-            ],
-          },
-        ],
-      },
-      {
-        text: { name: '2' },
-        children: [
-          {
-            text: { name: '3' },
-            children: [
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-            ],
-          },
-          {
-            text: { name: '3' },
-            children: [
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-            ],
-          },
-          {
-            text: { name: '3' },
-            children: [
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-              {
-                text: { name: '4' },
-              },
-            ],
-          },
-        ],
-      },
-    ],
+    text: { name: '4' },
+    children: a(4)
   },
 };
 
 
+function a(n) {
+  let arr = [];
+  let count = 0
+  for (var i = n - 1; i >= 0; i--) {
+    count++
+    arr.push({ text: { name: n*n  } , children:a(n-1) });
+  }
+  return arr;
+}
+
+// // console.log(a(3));
+// function b(n) {
+//   let arr = []
+//    for (var i = 0; i <= n; i++) {
+//      arr.push({ text: { name: i }, children: `${a(n-1)}` });
+//      a(n)
+//    }
+//   return arr
+// }
+// console.log(b(3));
