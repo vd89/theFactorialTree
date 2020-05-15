44f739494eaacd0e3286ed4a95904e7de1138dcf
vd89 <me@daxcodes.in>
2020-05-15 17:28:50 +0530

    The change in the final Stage

--- main.js
+++ main.js
@@ -1,41 +1,23 @@
+
+const node = a(4).pop()
+
 var chart_config = {
   chart: {
     container: '#tree-simple',
   },
 
-  nodeStructure: {
-    text: { name: "1" },
-    children: a(4)
-  },
+  nodeStructure: node
 };
 
 
-function a(num, n = 2) {
-  let arr = [];
-  let fact = 1
-  let count = 0
-  if(n <= num) {
-    // return;
+function a(num, n = 1) {
+  let arr = []; 
+  if(n <= num) {   
     for (var i =1; i<=n; i++) {
-      // fact *= i
-      count++
       arr.push({ text: { name: n }, children: a(num, n + 1) });
-      
     }
-    // console.log(arr);
     return arr;
   } else {
     return
   }
 }
-
-// // console.log(a(3));
-// function b(n) {
-//   let arr = []
-//    for (var i = n; i >= 0; i--) {
-//      arr.push({ text: { name: i }, children: `${a(n-1)}` });
-//      a(n)
-//    }
-//   return arr
-// }
-// console.log(b(3));
