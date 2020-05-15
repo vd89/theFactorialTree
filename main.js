5dd73d95ca8e09dc52673f66f6265aa7b300c1b8
vd89 <me@daxcodes.in>
2020-05-15 11:57:58 +0530

    the basic structure for the tree

--- main.js
+++ main.js
@@ -1,41 +1,125 @@
 var chart_config = {
-    chart: {
-        container: "#tree-simple"
-    },
+  chart: {
+    container: '#tree-simple',
+  },
 
-    nodeStructure: {
-        text: { name: "1" },
+  nodeStructure: {
+    text: { name: '1' },
+    children: [
+      {
+        text: { name: '2' },
         children: [
-            {
-                text: { name: "2" },
-                children: [
-                    {
-                        text: { name: "3" },
-                    },
-                    {
-                        text: { name: "3" },
-                    },
-                    {
-                        text: { name: "3" },
-                    }
-                ]
-            },
-            {
-                text: { name: "2" },
-                children: [
-                    {
-                        text: { name: "3" },
-                    },
-                    {
-                        text: { name: "3" },
-                    },
-                    {
-                        text: { name: "3" },
-                    }
-                ]
-            }
-        ]
-    }
+          {
+            text: { name: '3' },
+            children: [
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+            ],
+          },
+          {
+            text: { name: '3' },
+            children: [
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+            ],
+          },
+          {
+            text: { name: '3' },
+            children: [
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        text: { name: '2' },
+        children: [
+          {
+            text: { name: '3' },
+            children: [
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+            ],
+          },
+          {
+            text: { name: '3' },
+            children: [
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+            ],
+          },
+          {
+            text: { name: '3' },
+            children: [
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+              {
+                text: { name: '4' },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 };
 
 
